004252f7fe443b4074a32ed16a73af6b50065111
prof-eduardo-santos <edu.sigma@hotmail.com>
2026-04-23 20:01:21 -0300

    Nova URL Apps Script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function requisicao() {
 
-  fetch("https://script.google.com/macros/s/AKfycbz0RemQomNaW0aCebEGPLU9lNTD5bwgICaVX2YOVGN0b-WF2DLUk28p-OFMg06qmDYysQ/exec")
+  fetch("https://script.google.com/macros/s/AKfycbxKsj8vCoQObR3TWnR4gewk_OQHeHgnLO8ywJWdvwN8bB5rGOkc-lTONyp0nGO6mZPHzQ/exec")
     .then(res => res.text())
     .then(data => {
       document.getElementById("resposta").innerText = data;
