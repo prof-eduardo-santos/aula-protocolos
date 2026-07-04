38e529c96116bc673f74c1fbdfa5e954da075622
Prof Eduardo Santos <edu.sigma@hotmail.com>
2026-07-04 08:00:18 -0300

    Melhoria do CSS

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function requisicao() {
 
-  fetch("https://script.google.com/macros/s/AKfycbxKsj8vCoQObR3TWnR4gewk_OQHeHgnLO8ywJWdvwN8bB5rGOkc-lTONyp0nGO6mZPHzQ/exec")
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById("resposta").innerText = data;
-    })
-    .catch(error => {
-      document.getElementById("resposta").innerText = "Erro na requisição";
-      console.error(error);
-    });
-
-}
