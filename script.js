f0d553b2aed10d1a8d57420ba5991602066ac67d
Prof Eduardo Santos <edu.sigma@hotmail.com>
2026-07-05 18:21:21 -0300

    Mudanças na estrutura geral

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let zoom = 90;
 
+function alterarZoom(valor) {
+  zoom += valor;
+  zoom = Math.max(40, Math.min(250, zoom));
+  document.getElementById("infografico").style.width = zoom + "%";
+}
+
+function resetarZoom() {
+  zoom = 90;
+  document.getElementById("infografico").style.width = zoom + "%";
+}
